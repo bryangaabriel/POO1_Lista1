d52e4ec6f1ec4bcd6bded4c3a432f09eda51c942
Bryan <90529644+bryangaabriel@users.noreply.github.com>
2023-12-03 18:03:20 -0300

    Add files via upload

---
+++ Lista1 POO1/ex42.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1_POO1
+{
+    internal class ex42
+    {
+    }
+}
